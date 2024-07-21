55f05467de248e5d333bcecdc17ab51c9f62defe
Harry Tran <hai.tran@ffw.com>
2024-07-21 15:55:52 +0700

    feat(movie detail page): update structure

--- app/movie/[id]/page.tsx
+++ app/movie/[id]/page.tsx
@@ -1,7 +1,7 @@
 import { getDetailMovie } from "@/app/lib/data";
-import Image from "next/image";
+import MovieDetail from "@/app/components/MovieDetail";
 
-export default async function MovieDetail({
+export default async function MovieDetailPage({
   params,
 }: {
   params: { id: number };
@@ -10,41 +10,7 @@ export default async function MovieDetail({
 
   return (
     <>
-      <div className="flex flex-col md:flex-row">
-        <div className="w-100 h-[calc((100vw-40px)*3/2)] md:w-[20rem] md:h-[30rem] relative">
-          <Image
-            src={process.env.SMALL_IMG_PREFIX + movieData.poster_path}
-            alt={movieData.original_title}
-            style={{ objectFit: "contain" }}
-            fill
-            sizes="100%"
-          ></Image>
-        </div>
-        <div className="w-100 md:w-1/2 p-[20px]">
-          <h1 className="font-bold text-2xl pb-4">
-            {movieData.original_title}
-          </h1>
-          <p className="pb-4">
-            <i>{movieData.overview}</i>
-          </p>
-          <p className="pb-4">
-            <b>Genres: </b>
-            {movieData.genres.map(
-              (genre: { id: number; name: string }, index: number) => {
-                if (index !== movieData.genres.length - 1)
-                  return <span key={genre.id}>{`${genre.name}, `}</span>;
-                else return <span key={genre.id}>{genre.name}</span>;
-              }
-            )}
-          </p>
-          <p className="pb-4">
-            <b>Release date:</b> {movieData.release_date}
-          </p>
-          <p className="pb-4">
-            <b>Popularity:</b> {movieData.popularity}
-          </p>
-        </div>
-      </div>
+      <MovieDetail movieData={movieData} />
     </>
   );
 }
